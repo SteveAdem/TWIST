ba0b6be64e4ee030d22e2989285a5d6640ff5153
Steve Adem <syeveadem@hotmail.com>
2016-03-05 20:10:37 +0000

    gui

--- TWIST/Assets/Scripts/PlayerHealth.cs
+++ TWIST/Assets/Scripts/PlayerHealth.cs
@@ -7,7 +7,7 @@ public class PlayerHealth : MonoBehaviour
     public int startingHealth = 100;
     public int currentHealth;
     public Slider healthSlider;
-    //public Image damageImage;
+    public Image damageImage;
    // public AudioClip deathClip;
     public float flashSpeed = 5f;
     public Color flashColour = new Color(1f, 0f, 0f, 0.1f);
@@ -30,7 +30,14 @@ public class PlayerHealth : MonoBehaviour
 
     void Update()
     {
-
+        if (damaged)
+        {
+            damageImage.color = flashColour;
+        }
+        else
+        {
+            damageImage.color = Color.Lerp(damageImage.color, Color.clear, flashSpeed * Time.deltaTime);
+        }
         damaged = false;
     }
 
