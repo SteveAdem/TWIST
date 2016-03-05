3c92537825a2acac9fe67702479d6b24e72481b2
nuno.flores <nuno.flores@gmail.com>
2016-03-05 22:13:48 +0000

    Corrected PlayerHealth code.

--- TWIST/Assets/Scripts/PlayerHealth.cs
+++ TWIST/Assets/Scripts/PlayerHealth.cs
@@ -4,70 +4,59 @@ using System.Collections;
 
 public class PlayerHealth : MonoBehaviour
 {
-    public int startingHealth = 100;
-    public int currentHealth;
-    public Slider healthSlider;
-    public Image damageImage;
-   // public AudioClip deathClip;
-    public float flashSpeed = 5f;
-    public Color flashColour = new Color(1f, 0f, 0f, 0.1f);
+	public int startingHealth = 100;
+	public int currentHealth;
+	public Slider healthSlider;
+	public Image damageImage;
+	// public AudioClip deathClip;
+	public float flashSpeed = 5f;
+	public Color flashColour = new Color(1f, 0f, 0f, 0.1f);
 
-    //Animator anim;
-    //AudioSource playerAudio;
-    bool isDead;
-    bool damaged;
-
-
-    void Awake()
-    {
-        //anim = GetComponent<Animator>();
-        //playerAudio = GetComponent<AudioSource>();
-        currentHealth = startingHealth;
-    }
+	//Animator anim;
+	//AudioSource playerAudio;
+	bool isDead;
+	bool damaged;
 
 
-    void Update()
-    {
-<<<<<<< HEAD
-=======
-        if (damaged)
-        {
-            damageImage.color = flashColour;
-        }
-        else
-        {
-            damageImage.color = Color.Lerp(damageImage.color, Color.clear, flashSpeed * Time.deltaTime);
-        }
->>>>>>> 5236753f0517f0cf515eabe25475e3a08863b3ad
-        damaged = false;
-    }
+	void Awake()
+	{
+		//anim = GetComponent<Animator>();
+		//playerAudio = GetComponent<AudioSource>();
+		currentHealth = startingHealth;
+	}
 
-    public void TakeDamage(int amount)
-    {
-        damaged = true;
-        currentHealth -= amount;
-        healthSlider.value = currentHealth;
+
+	void Update()
+	{
+		damaged = false;
+	}
+
+	public void TakeDamage(int amount)
+	{
+		damaged = true;
+		currentHealth -= amount;
+		healthSlider.value = currentHealth;
 
 		Debug.Log ("health:" + currentHealth);
 
-        //playerAudio.Play();
+		//playerAudio.Play();
 
-        if (currentHealth <= 0 && !isDead)
-        {
-            Death();
-        }
-    }
+		if (currentHealth <= 0 && !isDead)
+		{
+			Death();
+		}
+	}
 
 
-    void Death()
-    {
-        isDead = true;
+	void Death()
+	{
+		isDead = true;
 
 		Debug.Log ("DEAD!!!");
-        //anim.SetTrigger("Die");
+		//anim.SetTrigger("Die");
 
-        //playerAudio.clip = deathClip;
-        //playerAudio.Play();
+		//playerAudio.clip = deathClip;
+		//playerAudio.Play();
 
-    }
+	}
 }
